cb63cbc2eb5aa85770dbdae5e9f1816b3301e8d5
Dayne Xu <crane109937@gmail.com>
2026-04-25 23:17:17 +0800

    1

--- short_memory/llm_context/01_trim_message_code.py
+++ short_memory/llm_context/01_trim_message_code.py
@@ -5,7 +5,7 @@ from langchain_core.tools import tool
 from langgraph.checkpoint.memory import InMemorySaver
 from langgraph.runtime import Runtime
 
-from init_llm import deepseek_llm
+from init_llm import ark_llm
 
 
 @tool
@@ -22,9 +22,9 @@ def get_weather(city: str) -> str:
 
 
 @before_model
-def trim_message(state:AgentState,runtime:Runtime) -> dict|None:
-    print("当前state:",state)
-    print("当前runtime:",runtime)
+def trim_message(state: AgentState, runtime: Runtime) -> dict | None:
+    print("当前state:", state)
+    print("当前runtime:", runtime)
 
     messages = state["messages"]
     if len(messages) > 5:
@@ -33,7 +33,7 @@ def trim_message(state:AgentState,runtime:Runtime) -> dict|None:
         if messages[-retain_msg_count].type == "tool":
             retain_msg_count = 2
 
-        print("保留的消息:",messages[-retain_msg_count:])
+        print("保留的消息:", messages[-retain_msg_count:])
 
         return {"messages": [RemoveMessage(id=msg.id) for msg in messages[:-retain_msg_count]]}
 
@@ -41,25 +41,23 @@ def trim_message(state:AgentState,runtime:Runtime) -> dict|None:
 
 
 agent = create_agent(
-    model=deepseek_llm,
+    model=ark_llm,
     tools=[get_weather],
     middleware=[trim_message],
     checkpointer=InMemorySaver()
 )
-
 
 config = {"configurable": {"thread_id": "session_001"}}
 
 # 模拟对话
 response1 = agent.invoke({"messages": [{"role": "user", "content": "你好，我是张三"}]}, config=config)
 print(response1["messages"][-1].content)
-print("***"*20)
+print("***" * 20)
 response2 = agent.invoke({"messages": [{"role": "user", "content": "今天北京天气好吗？"}]}, config=config)
 print(response2["messages"][-1].content)
-print("***"*20)
+print("***" * 20)
 response3 = agent.invoke({"messages": [{"role": "user", "content": "上海天气怎么样？"}]}, config=config)
 print(response3["messages"][-1].content)
-print("***"*20)
+print("***" * 20)
 final_response = agent.invoke({"messages": [{"role": "user", "content": "我的名字叫什么？"}]}, config=config)
 print(final_response["messages"][-1].content)
-
